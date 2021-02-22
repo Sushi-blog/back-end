cf4178ff21d5a2df6a93e32d7955d8b891ef0d42
이승윤 <yyuunn17@naver.com>
2021-02-22 15:02:29 +0900

    [UPDATE] http config 추가

--- src/main/java/com/sushiblog/backend/security/config/SecurityConfig.java
+++ src/main/java/com/sushiblog/backend/security/config/SecurityConfig.java
@@ -1,17 +1,51 @@
 package com.sushiblog.backend.security.config;
 
 
+import com.sushiblog.backend.security.jwt.JwtConfigure;
+import com.sushiblog.backend.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() //웹사이트 취약점 공격
+                .cors().and()
+                .sessionManagement().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/account").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .anyRequest().authenticated().and()
+                .apply(new JwtConfigure(jwtTokenProvider));
+
+        http.logout() // 만약 csrf와 사용하는 경우 무조건 post
+                .logoutUrl("/logout") //로그아웃 요청 시 사용하는 URL
+                .logoutSuccessUrl("/auth"); // 로그아웃 성공시 반환하는 URL
+
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
