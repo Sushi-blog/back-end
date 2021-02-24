37c782f37013755f94db1db2845d0f121171e67b
이승윤 <yyuunn17@naver.com>
2021-02-24 15:30:13 +0900

    [UPDATE]

--- src/main/java/com/sushiblog/backend/service/auth/AuthServiceImpl.java
+++ src/main/java/com/sushiblog/backend/service/auth/AuthServiceImpl.java
@@ -7,7 +7,6 @@ import com.sushiblog.backend.entity.user.UserRepository;
 import com.sushiblog.backend.error.UserNotFoundException;
 import com.sushiblog.backend.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,19 +19,13 @@ public class AuthServiceImpl implements AuthService {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    @Value("${auth.jwt.exp.refresh}")
-    private Long refreshExp;
-
-    @Value("${auth.jwt.exp.access}")
-    private Long accessExp;
-
 
     @Override
     public TokenResponse signIn(SignInRequest request) {
         return userRepository.findById(request.getEmail())
                 .filter(user -> passwordEncoder.matches(request.getPassword(), user.getPassword()))
                 .map(User::getEmail)
-                .map(refreshToken -> {
+                .map(a -> {
                     String accessToken = jwtTokenProvider.generateAccessToken(request.getEmail());
                     return new TokenResponse(accessToken);
                 })
