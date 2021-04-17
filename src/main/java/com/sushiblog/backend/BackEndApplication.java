e4663b47bbef1a4f274b60c2aaa4a1bc71706406
이승윤 <yyuunn17@naver.com>
2021-04-17 22:09:04 +0900

    [UPDATE]

--- src/main/java/com/sushiblog/backend/BackEndApplication.java
+++ src/main/java/com/sushiblog/backend/BackEndApplication.java
@@ -2,9 +2,8 @@ package com.sushiblog.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class BackEndApplication {
 
 	public static void main(String[] args) {
